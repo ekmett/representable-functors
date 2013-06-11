ab228b14c5f57cf5acf5aac0cd86952ed7a5a96f
Edward Kmett <ekmett@gmail.com>
2013-06-11 13:46:12 -0400

    Support Day convolution

--- src/Data/Functor/Contravariant/Representable.hs
+++ src/Data/Functor/Contravariant/Representable.hs
@@ -29,6 +29,7 @@ module Data.Functor.Contravariant.Representable
 
 import Control.Monad.Reader
 import Data.Functor.Contravariant
+import Data.Functor.Contravariant.Day
 import Data.Functor.Product
 import Data.Functor.Coproduct
 import Prelude hiding (lookup)
@@ -39,14 +40,30 @@ type family Value (f :: * -> *)
 class Contravariant f => Valued f where
   contramapWithValue :: (b -> Either a (Value f)) -> f a -> f b
 
+instance (Valued f, Valued g) => Valued (Day f g) where
+  contramapWithValue d2eafg (Day fb gc abc) = Day (contramapWithValue id fb) (contramapWithValue id gc) $ \d -> case d2eafg d of
+    Left a -> case abc a of
+      (b, c) -> (Left b, Left c)
+    Right (vf, vg) -> (Right vf, Right vg)
+
 -- | Dual to 'Indexed'.
 class Coindexed f where
   coindex :: f a -> a -> Value f
+
+type instance Value (Day f g) = (Value f, Value g)
+
+instance (Coindexed f, Coindexed g) => Coindexed (Day f g) where
+  coindex (Day fb gc abc) a = case abc a of
+    (b, c) -> (coindex fb b, coindex gc c)
 
 -- | A 'Contravariant' functor @f@ is 'Representable' if 'contrarep' and 'coindex' witness an isomorphism to @(_ -> Value f)@.
 class (Coindexed f, Valued f) => Representable f where
   -- | > contramap f (contrarep g) = contrarep (g . f)
   contrarep :: (a -> Value f) -> f a
+
+instance (Representable f, Representable g) => Representable (Day f g) where
+  contrarep a2fg = Day (contrarep fst) (contrarep snd) $ \a -> let b = a2fg a in (b,b)
+  {-# INLINE contrarep #-}
 
 {-# RULES
 "contrarep/coindex" forall t. contrarep (coindex t) = t
